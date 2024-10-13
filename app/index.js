8c13b8ed02a0296130b642cc0b66b7c090c49654
unknown <unekue2@gmail.com>
2024-10-13 15:30:41 +0100

    intial setup, colours theme sizes

---
+++ app/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+function index(props) {
+  return (
+    <View style={styles.container}>
+      <Text>welcome</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default index;
